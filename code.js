d39a6ad4cb610536eff920e0ea74dde6ac6efee8
saturnLang <95869708+saturnLang@users.noreply.github.com>
2022-01-24 18:38:39 -0500

    Update code.js

--- code.js
+++ code.js
@@ -1,5 +1,3 @@
-function includeCookies(){
-  var script = document.createElement("script");
-  script.src="https://saturnlang.github.io/markuplanguage/cookies.js";
-  document.body.appendChild(script);
-}
+var script = document.createElement("script");
+script.src="https://saturnlang.github.io/markuplanguage/cookies.js";
+document.body.appendChild(script);
