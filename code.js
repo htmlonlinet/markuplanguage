6379508c80ded249c1b692b52d559b283a7c7dc3
saturnLang <95869708+saturnLang@users.noreply.github.com>
2022-01-24 18:34:55 -0500

    Update code.js

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 function includeCookies(){
   var script = document.createElement("script");
-  script.src="cookies.js";
+  script.src="https://saturnlang.github.io/markuplanguage/cookies.js";
   document.body.appendChild(script);
 }
