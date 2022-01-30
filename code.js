6d426601bcb42f174cdc19616b0e040bbcbdc869
saturnLang <95869708+saturnLang@users.noreply.github.com>
2022-01-30 11:35:04 -0500

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,5 @@
 var script = document.createElement("script");
 script.src="https://saturnlang.github.io/markuplanguage/cookies.js";
 document.body.appendChild(script);
+var header = document.getElementsByTagName("header")[0];
+header.innerHTML = "<h1>" + header.innerText; + "</header>";
