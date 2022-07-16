d10e97fc73bd1782c963b2f762b710de24c5f35b
saturnLang <95869708+saturnLang@users.noreply.github.com>
2022-07-16 16:02:05 -0400

    Update code.js

--- code.js
+++ code.js
@@ -5,3 +5,5 @@ var header = document.getElementsByTagName("header")[0];
 header.innerHTML = "<h1>" + header.innerText; + "</h1>";
 var pagename = document.getElementsByTagName("pagename")[0];
 pagename.innerHTML = "<title>" + pagename.innerText; + "</title>";
+var scrip = document.getElementsByTagName("jscript")[0];
+scrip.innerHTML ="<script>" + scrip.innerText + "</script>";
